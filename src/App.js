7a9bd09c051d8b02eb5c5663f4524a8a9cbba8ad
Alexis Olveres <alexis.olv.developer@gmail.com>
2018-10-20 02:57:57 -0500

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import {Helmet} from 'react-helmet'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+      <Helmet>
+      <title>Si Cambio</title>
+
+      <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+      <meta property="og:type"               content="article" />
+      <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+      <meta property="og:description"        content="How much does culture influence creative thinking?" />
+      <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+      </Helmet>
+
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
